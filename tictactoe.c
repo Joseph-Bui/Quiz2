6551222e0f6007979d6d22399183e71453cae1e2
Joseph Bui <jhbui@cpp.edu>
2022-10-07 18:42:01 -0700

    initial comments on what to code

---
+++ tictactoe.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+int main()
+{
+    //prompt user to choose game mode
+    
+    //print the board and prompt user to play. 
+
+
+    //repeat until there is a winner or draw.
+
+    return 0;
+}
