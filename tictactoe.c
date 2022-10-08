5a0fb21cd58f877981f5cd4ced8ccc850d261771
Joseph Bui <jhbui@cpp.edu>
2022-10-07 18:45:48 -0700

    visualizing functions to be used

--- tictactoe.c
+++ tictactoe.c
@@ -3,14 +3,51 @@
 #include <ctype.h>
 #include <time.h>
 
+void restartGame();
+void printBoard();
+int checkSpace();
+void playersMove();
+void computersMove();
+char checkWinner();
+void printWinnter(char);
+
 int main()
 {
     //prompt user to choose game mode
-    
+
     //print the board and prompt user to play. 
 
 
     //repeat until there is a winner or draw.
 
     return 0;
+}
+
+void restartGame()
+{
+
+}
+void printBoard()
+{
+
+}
+int checkSpace()
+{
+
+}
+void playersMove()
+{
+
+}
+void computersMove()
+{
+
+}
+char checkWinner()
+{
+
+}
+void printWinnter(char winner)
+{
+    
 }
